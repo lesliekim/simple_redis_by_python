8f40b90676226745de27a174a3f3839448e76116
lesliekim <shulanpan@qq.com>
2018-05-25 17:14:36 +0800

    add protocal handler

--- server.py
+++ server.py
@@ -6,11 +6,42 @@ class Disconnect(Exception): pass
 class CommandError(Exception): pass
 
 class ProtocolHandler(object):
+    # redis protocal: https://redis.io/topics/protocol
+    def __init__(self):
+        self.handler_map = {
+            "+": self.handle_simple_string,
+            "-": self.handle_errors,
+            ":": self.handle_integers,
+            "$": self.handle_bulk_strings,
+            "*": self.handle_arrays
+        }
     def handle_request(self, socket_file):
         pass
 
     def write_response(self, socket_file, data):
         pass
+
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip()
+
+    def handle_errors(self, socket_file):
+        pass
+
+    def handle_integers(self, socket_file):
+        return int(socket_file.readline().rstrip())
+
+    # for a single binary safe string
+    def handle_bulk_strings(self, socket_file):
+        num = int(socket_file.readline().rstrip())
+        bulk_string = socket_file.read(num + 2)
+        return bulk_string[:-2]
+
+    def handle_arrays(self, socket_file):
+        array_length = int(socket_file.readline().rstrip())
+        result = []
+        for i in range array_length:
+            result.append(self.handle_request(socket_file))
+        return result
 
 class Server(object):
     def __init__(self, host="127.0.0.1", port="3000", max_client=64):
@@ -41,7 +72,7 @@ class Server(object):
 
     def get_response(self, data):
         pass
-    
+
     def run(self):
         self._server.serve_forever()
-        
+
